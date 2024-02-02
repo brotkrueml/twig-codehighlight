06afb9a6a3bc39b8dc098abf7bfb86f9295cfc78
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-02-02 18:49:22 +0100

    chore: update rector to 0.19.5

--- rector.php
+++ rector.php
@@ -3,23 +3,22 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
-use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
+use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $config): void {
     $config->phpVersion(PhpVersion::PHP_81);
 
     $config->import(LevelSetList::UP_TO_PHP_81);
+    $config->import(PHPUnitSetList::PHPUNIT_100);
+    $config->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
     $config->import(SetList::CODE_QUALITY);
     $config->import(SetList::DEAD_CODE);
     $config->import(SetList::EARLY_RETURN);
     $config->import(SetList::TYPE_DECLARATION);
-    $config->import(PHPUnitLevelSetList::UP_TO_PHPUNIT_100);
-    $config->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
 
     $config->autoloadPaths([
         __DIR__ . '/vendor/autoload.php',
